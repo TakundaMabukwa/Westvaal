16347381173db1f3e5363f8be3e7fd8492777031
TakundaMabukwa <mabukwatakunda@gmail.com>
2026-02-10 19:15:24 +0200

    updated start port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,18 @@ module.exports = {
   apps: [{
     name: 'westvaal-dashboard',
     script: 'node_modules/next/dist/bin/next',
-    args: 'start',
+    args: 'start -p 3001',
     cwd: '/var/www/Westvaal',
     instances: 1,
     exec_mode: 'cluster',
+    env_file: '/var/www/Westvaal/.env.local',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000
-    }
+      PORT: 3001,
+      HOSTNAME: '0.0.0.0'
+    },
+    error_file: '/var/www/Westvaal/logs/error.log',
+    out_file: '/var/www/Westvaal/logs/out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
   }]
 }
